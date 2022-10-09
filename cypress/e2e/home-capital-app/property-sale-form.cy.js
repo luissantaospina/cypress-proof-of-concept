54cd54333fcc25032b4a1cd97a01ed39ce520611
Luis <luisfernandosantaospina@gmail.com>
2022-10-09 11:13:58 -0500

    Agrega test formulario de vender hasta paso 3

--- cypress/e2e/home-capital-app/property-sale-form.cy.js
+++ cypress/e2e/home-capital-app/property-sale-form.cy.js
@@ -7,7 +7,23 @@ describe('Home Capital App - Sale', () => {
         cy.get('[id="the-header--purchase-cases-link-desktop"]').click()
         cy.contains('efectivo')
     })
-    it('start property sale form', () => {
+    it('start property sale form step 1', () => {
         cy.get('[id="purchase-case-start-request-offer-survey--city"]').click()
+        cy.contains('Bello').click()
+        cy.get('[id="purchase-case-start-request-offer-survey--address-road-type"]').click()
+        cy.contains('Carrera').click()
+        cy.get('[id="purchase-case-start-request-offer-survey--address-road-number"]').type('59b')
+        cy.get('[id="purchase-case-start-request-offer-survey--address-generator-road"]').type('58')
+        cy.get('[id="purchase-case-start-request-offer-survey--address-generator-road-number"]').type('51')
+        cy.get('[id="purchase-case-start-request-offer-survey--next"]').click()
+    })
+    it('start property sale form step 2', () => {
+        cy.contains('Cra. 59b #51-58, Bello, Antioquia, Colombia')
+        cy.get('[id="request-offer-survey-location--next"]').click()
+    })
+    it('start property sale form step 3', () => {
+        cy.get('[id="request-offer-survey-personal-information--name-group-1"]').type('Luis')
+        cy.get('[id="request-offer-survey-personal-information--last-name-group-1"]').type('Santa')
+        cy.get('[id="request-offer-survey-personal-information--email-group-1"]').type('hola@mundo.co')
     })
 })
